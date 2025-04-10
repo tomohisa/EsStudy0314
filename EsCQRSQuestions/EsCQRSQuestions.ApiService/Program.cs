9b1aa189476e7a47695a8671945fba614c0b8566
Tomohisa Takaoka <ttakaoka@jtechs.com>
2025-04-09 22:02:44 -0700

    feat: Enhance Question Management with Group Selection and API Integration

--- EsCQRSQuestions/EsCQRSQuestions.ApiService/Program.cs
+++ EsCQRSQuestions/EsCQRSQuestions.ApiService/Program.cs
@@ -5,6 +5,8 @@ using EsCQRSQuestions.Domain;
 using EsCQRSQuestions.Domain.Aggregates.ActiveUsers.Queries;
 using EsCQRSQuestions.Domain.Aggregates.Questions.Commands;
 using EsCQRSQuestions.Domain.Aggregates.Questions.Queries;
+using EsCQRSQuestions.Domain.Aggregates.QuestionGroups.Commands;
+using EsCQRSQuestions.Domain.Aggregates.QuestionGroups.Queries;
 using EsCQRSQuestions.Domain.Aggregates.WeatherForecasts.Commands;
 using EsCQRSQuestions.Domain.Generated;
 using Orleans.Storage;
@@ -277,6 +279,123 @@ apiRoute.MapGet("/activeusers/{id}", async (Guid id, [FromServices]SekibanOrlean
     })
     .WithOpenApi()
     .WithName("GetActiveUsers");
+
+// QuestionGroups API endpoints
+// Queries
+apiRoute.MapGet("/questionGroups", async ([FromServices]SekibanOrleansExecutor executor) =>
+    {
+        var list = await executor.QueryAsync(new GetQuestionGroupsQuery()).UnwrapBox();
+        return list.Items;
+    })
+    .WithOpenApi()
+    .WithName("GetQuestionGroups");
+
+apiRoute.MapGet("/questionGroups/{id}", async (Guid id, [FromServices]SekibanOrleansExecutor executor) =>
+    {
+        var groups = await executor.QueryAsync(new GetQuestionGroupsQuery()).UnwrapBox();
+        var group = groups.Items.FirstOrDefault(g => g.Id == id);
+        if (group == null)
+        {
+            return Results.NotFound();
+        }
+        return Results.Ok(group);
+    })
+    .WithOpenApi()
+    .WithName("GetQuestionGroupById");
+
+apiRoute.MapGet("/questionGroups/{id}/questions", async (Guid id, [FromServices]SekibanOrleansExecutor executor) =>
+    {
+        var questions = await executor.QueryAsync(new GetQuestionsByGroupIdQuery(id)).UnwrapBox();
+        return questions.Items;
+    })
+    .WithOpenApi()
+    .WithName("GetQuestionsByGroupId");
+
+// Commands
+apiRoute
+    .MapPost(
+        "/questionGroups",
+        async (
+            [FromBody] CreateQuestionGroup command,
+            [FromServices] SekibanOrleansExecutor executor) => await executor.CommandAsync(command).UnwrapBox())
+    .WithOpenApi()
+    .WithName("CreateQuestionGroup");
+
+apiRoute
+    .MapPut(
+        "/questionGroups/{id}",
+        async (
+            Guid id,
+            [FromBody] UpdateQuestionGroupName command,
+            [FromServices] SekibanOrleansExecutor executor) => 
+        {
+            if (id != command.QuestionGroupId)
+            {
+                return Results.BadRequest("ID in URL does not match ID in command");
+            }
+            return Results.Ok(await executor.CommandAsync(command).UnwrapBox());
+        })
+    .WithOpenApi()
+    .WithName("UpdateQuestionGroup");
+
+apiRoute
+    .MapDelete(
+        "/questionGroups/{id}",
+        async (
+            Guid id,
+            [FromServices] SekibanOrleansExecutor executor) => 
+        {
+            var command = new DeleteQuestionGroup(id);
+            return Results.Ok(await executor.CommandAsync(command).UnwrapBox());
+        })
+    .WithOpenApi()
+    .WithName("DeleteQuestionGroup");
+
+apiRoute
+    .MapPost(
+        "/questionGroups/{id}/questions",
+        async (
+            Guid id,
+            [FromBody] AddQuestionToGroup command,
+            [FromServices] SekibanOrleansExecutor executor) => 
+        {
+            if (id != command.QuestionGroupId)
+            {
+                return Results.BadRequest("Group ID in URL does not match ID in command");
+            }
+            return Results.Ok(await executor.CommandAsync(command).UnwrapBox());
+        })
+    .WithOpenApi()
+    .WithName("AddQuestionToGroup");
+
+apiRoute
+    .MapPut(
+        "/questionGroups/{groupId}/questions/{questionId}/order",
+        async (
+            Guid groupId,
+            Guid questionId,
+            [FromBody] int newOrder,
+            [FromServices] SekibanOrleansExecutor executor) => 
+        {
+            var command = new ChangeQuestionOrder(groupId, questionId, newOrder);
+            return Results.Ok(await executor.CommandAsync(command).UnwrapBox());
+        })
+    .WithOpenApi()
+    .WithName("ChangeQuestionOrder");
+
+apiRoute
+    .MapDelete(
+        "/questionGroups/{groupId}/questions/{questionId}",
+        async (
+            Guid groupId,
+            Guid questionId,
+            [FromServices] SekibanOrleansExecutor executor) => 
+        {
+            var command = new RemoveQuestionFromGroup(groupId, questionId);
+            return Results.Ok(await executor.CommandAsync(command).UnwrapBox());
+        })
+    .WithOpenApi()
+    .WithName("RemoveQuestionFromGroup");
 
 // System administration endpoints
 apiRoute
